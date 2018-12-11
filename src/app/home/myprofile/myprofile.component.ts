b5d2bf103a98e510f3773ae45fc147a0d4b64a88
oreon <singhjess@gmail.com>
2018-12-11 13:56:58 -0500

    fixed profile

--- src/app/home/myprofile/myprofile.component.ts
+++ src/app/home/myprofile/myprofile.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { ChildService } from '@app/child/child.service';
-import { Observable } from 'rxjs';
-import { Child } from '@app/child/child';
+import { Component, OnInit } from "@angular/core";
+import { ChildService } from "@app/child/child.service";
+import { Observable } from "rxjs";
+import { Child } from "@app/child/child";
+import { ParentService } from "@app/parent/parent.service";
+import { Parent } from "@app/parent/parent";
+import { AuthenticationService } from "@app/core";
 
 @Component({
-  selector: 'app-myprofile',
-  templateUrl: './myprofile.component.html',
-  styleUrls: ['./myprofile.component.scss']
+  selector: "app-myprofile",
+  templateUrl: "./myprofile.component.html",
+  styleUrls: ["./myprofile.component.scss"]
 })
 export class MyprofileComponent implements OnInit {
-  entities$: Observable<Child[]>
+  entities$: Observable<Child[]>;
+  entity: Parent;
 
-  constructor(private childService: ChildService) { }
+  constructor(private childService: ChildService,
+    private parentService: ParentService,
+    private authenticationService: AuthenticationService) {
+    parentService.getById(
+      this.authenticationService.localUser.uid).subscribe(
+        x => this.entity = x
+      );
+  }
+
+  onSubmit(e: any) {
+    this.parentService.upsert(this.authenticationService.localUser.uid, e);
+  }
 
   ngOnInit() {
-    this.entities$ = this.childService.getMyKids()
+    this.entities$ = this.childService.getMyKids();
   }
 
   unsponsor(event: any) {
-    this.childService.unsponsor(event)
+    this.childService.unsponsor(event);
   }
-
 }
